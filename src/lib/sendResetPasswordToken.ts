700765357afb17d14edf9967e186b879161a6b45
emilpoppler134 <emil@50archive.com>
2024-03-12 09:13:10 +0100

    Change the subject in automated mail

--- src/lib/sendResetPasswordToken.ts
+++ src/lib/sendResetPasswordToken.ts
@@ -25,9 +25,9 @@ export async function sendResetPasswordToken(email: string, name: string, code: 
   } as SMTPTransport.Options);
 
   const mailOptions = {
-    from: '50archive <50archivese@gmail.com>',
+    from: 'Netlight <emil.poppler@gmail.com>',
     to: email,
-    subject: "Order Confirmation",
+    subject: "Forgot Password",
     html: `
       <div style="width: 100%; height: 100%; margin: 0; background-color: #f6f9fc;">
         <div style="width: 100%; max-width: 600px; margin: 0 auto; background-color: #fff; border-bottom-left-radius: 10px; border-bottom-right-radius: 10px;">
